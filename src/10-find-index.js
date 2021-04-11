73e371e25ec727d62e4ad00dbc1fb4067dee781f
Oleksii Korovushkin <alexkrv07@gmail.com>
2021-04-11 19:21:52 +0300

    feat: implement solve task 10

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,27 +11,25 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  // let begin = 0;
-  // let end = array.length;
-  // let mid = Math.floor((end - begin) / 2);
+function findIndex(array, value) {
+  let begin = 0;
+  let end = array.length;
+  let mid = Math.floor((end - begin) / 2);
 
-  // while (begin !== end) {
-  //   if (array[mid] === value) {
-  //     return mid;
-  //   }
-  //   if (array[mid] < value) {
-  //     begin = mid + 1;
-  //     // return mid + findIndex(array.slice(begin, end), value);
-  //   } else {
-  //     end = mid;
-  //   }
-  //   mid = Math.floor((end - begin) / 2);
-  // }
-  // return -1;
-  // end = mid;
-  // return array[mid];
-  throw new Error('Not implemented');
+  while (begin < end) {
+    if (array[mid] === value) {
+      return mid;
+    }
+    if (array[mid] < value) {
+      begin = mid + 1;
+      mid = begin + Math.floor((end - begin) / 2);
+    } else {
+      end = mid;
+      mid = begin + Math.floor((end - begin) / 2);
+    }
+  }
+  return -1;
+  // throw new Error('Not implemented');
 }
 
 module.exports = findIndex;
