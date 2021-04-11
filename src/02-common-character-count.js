09f3908108ae8a4ca6f12990ef0376eec99df3ba
Oleksii Korovushkin <alexkrv07@gmail.com>
2021-04-11 19:50:49 +0300

    feat: implement task 02

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -9,33 +9,35 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  // const temp1 = {};
-  // const temp2 = {};
-  // for (let i = 0; i < s1.length; i++) {
-  //   if (!temp1[s1[i]]) {
-  //     temp1[s1[i]] = 1;
-  //   } else {
-  //     temp1[s1[i]]++;
-  //   }
-  // }
-  // for (let i = 0; i < s2.length; i++) {
-  //   if (!temp2[s2[i]]) {
-  //     temp2[s2[i]] = 1;
-  //   } else {
-  //     temp2[s2[i]]++;
-  //   }
-  // }
-  // let sum = 0;
-  // const key1 = Object.keys(temp1);
-  // const key2 = Object.keys(temp2);
-  // for (let i = 0; i < key1.length; i++) {
-  //   if (key1[i] in key2) {
-  //     sum += temp1[key1[i]] < temp2[key1[i]] ? temp1[key1[i]] : temp2[key1[i]];
-  //   }
-  // }
-  // return sum;
-  throw new Error('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  const temp1 = {};
+  const temp2 = {};
+  for (let i = 0; i < s1.length; i++) {
+    if (!temp1[s1[i]]) {
+      temp1[s1[i]] = 1;
+    } else {
+      temp1[s1[i]]++;
+    }
+  }
+  for (let i = 0; i < s2.length; i++) {
+    if (!temp2[s2[i]]) {
+      temp2[s2[i]] = 1;
+    } else {
+      temp2[s2[i]]++;
+    }
+  }
+
+  let sum = 0;
+  const key1 = Object.keys(temp1);
+  const key2 = Object.keys(temp2);
+
+  for (let i = 0; i < key1.length; i++) {
+    if (key2.includes(key1[i])) {
+      sum += temp1[key1[i]] < temp2[key1[i]] ? temp1[key1[i]] : temp2[key1[i]];
+    }
+  }
+  return sum;
+  // throw new Error('Not implemented');
 }
 
 module.exports = getCommonCharacterCount;
